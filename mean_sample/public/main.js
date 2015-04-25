5b572d7f711b6e7c519da6cc03a9a9e7ac4492dc
Jake Austin <jakeaust07@gmail.com>
2015-04-25 16:17:48 -0400

    create/delete todos

--- mean_sample/public/main.js
+++ mean_sample/public/main.js
@@ -1,6 +1,31 @@
 var module = angular.module('todoApp', []);
 
 module.controller('mainController', function($scope, $http) {
+  $scope.formData = {};
+
+  $scope.createTodo = function() {
+    $http.post('/api/todos', $scope.formData).success(function(d) {
+      $scope.formData = {};
+      $scope.todos = d;
+    }).error(function(e) {
+      console.log('Error ' + e);
+    });
+  };
+
+  $scope.deleteTodo = function(id) {
+    $http.delete('api/todos/' + id)
+    .success(function(d) {
+      $scope.todos = d;
+    }).error(function(e) {
+      console.log('Error ' + e);
+    });
+  };
+  $http.get('/api/todos').success(function(d) {
+    $scope.todos = d;
+  }).error(function(e) {
+    console.log('Error ' + e);
+  });
+
 
 });
 
